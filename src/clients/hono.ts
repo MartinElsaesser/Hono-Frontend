46a50b0974685268f7851431ed92c7677d092c54
Martin Elsässer <marels@aon.at>
2025-05-15 18:44:42 +0200

    change import path

--- src/clients/hono.ts
+++ src/clients/hono.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
-import type { ApiRoutes } from "../../../Backend/src/server";
+import type { ApiRoutes } from "../../../Hono-Backend/src/server";
 
 export const honoClient = hc<ApiRoutes>("http://localhost:3000");
