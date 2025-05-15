24a9f800440fb51b4f6a160375df5d2f3d092e85
Martin Elsässer <marels@aon.at>
2025-05-15 17:10:20 +0200

    wip: toggle done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { honoClient } from "./clients/hono";
 import { useHono } from "./hooks/useHono";
 
 const getAllTodos = honoClient.api.todos.$get;
+type Todo = {
+    id: number;
+    created_at: string;
+    description: string;
+    done: boolean;
+    headline: string;
+    position: number;
+}
 
 function App() {
   const {data: todos, isLoading, error, mutate} = useHono({
@@ -13,10 +21,20 @@ function App() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
 
+  const toggleDone = async (todo: Todo) => {
+    const response = await honoClient.api.todos[":todoId"].$patch({
+      param: { todoId: todo.id.toString() },
+      json: {
+        done: !todo.done,
+      },
+    });
+    mutate();
+  }
+
   const todoList = todos?.data.todos.map((todo) => (
     <div key={todo.id} className="todo-card">
       <h3>
-        <input type="checkbox" checked={todo.done} />
+        <input type="checkbox" checked={todo.done} onChange={() => toggleDone(todo)}/>
         {todo.headline}
       </h3>
       <div>{todo.description}</div>
