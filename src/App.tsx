32beb111bfcdbc029258a95082d097a8ffa6d330
Martin Elsaesser <marels@aon.at>
2025-05-15 19:49:33 +0200

    use new types

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { useHono } from "./hooks/useHono";
 
 const getAllTodos = honoClient.api.todos.$get;
 type Todo = {
-    id: number;
-    created_at: string;
-    description: string;
-    done: boolean;
-    headline: string;
-    position: number;
-}
+  id: number;
+  created_at: string;
+  description: string;
+  done: boolean;
+  headline: string;
+  position: number;
+};
 
 function App() {
-  const {data: todos, isLoading, error, mutate} = useHono({
+  const { data: todos, isLoading, error, mutate } = useHono({
     input: {},
     endpoint: getAllTodos,
   });
@@ -22,34 +22,38 @@ function App() {
   if (error) return <div>Error</div>;
 
   const toggleDone = async (todo: Todo) => {
+    const optimisticData = todos!.map((t) => {
+      const updateDone = t.id == todo.id ? !t.done : t.done;
+      return { ...t, done: updateDone };
+    });
+    mutate(
+      async () => {
+        await honoClient.api.todos[":todoId"].$patch({
+          param: { todoId: todo.id.toString() },
+          json: {
+            done: !todo.done,
+          },
+        });
+        const allTodosResponse = await honoClient.api.todos.$get({});
+        return await allTodosResponse.json();
+      },
+      {
+        optimisticData,
+        rollbackOnError: true,
+        revalidate: false,
+        throwOnError: true,
+      }
+    );
+  };
 
-    let optimisticData =  structuredClone(todos!)
-    optimisticData.data.todos = optimisticData?.data.todos.map(t => {
-
-            if(t.id == todo.id) {
-              return {
-                ...t,
-                done: !t.done
-              }
-            }
-            return t;
-          })
-    mutate(async (data) => {
-      const response = await honoClient.api.todos[":todoId"].$patch({
-        param: { todoId: todo.id.toString() },
-        json: {
-          done: !todo.done,
-        },
-      });
-      const allTodosResponse= await honoClient.api.todos.$get({});
-      return await allTodosResponse.json();
-    }, {optimisticData, rollbackOnError: true, revalidate: false, throwOnError: true});
-  }
-
-  const todoList = todos?.data.todos.map((todo) => (
+  const todoList = todos!.map((todo) => (
     <div key={todo.id} className="todo-card">
       <h3>
-        <input type="checkbox" checked={todo.done} onChange={() => toggleDone(todo)}/>
+        <input
+          type="checkbox"
+          checked={todo.done}
+          onChange={() => toggleDone(todo)}
+        />
         {todo.headline}
       </h3>
       <div>{todo.description}</div>
