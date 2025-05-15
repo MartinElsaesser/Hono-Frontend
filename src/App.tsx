4866fbf3aeec8574159396027bb6004dd32bb9df
Martin Elsässer <marels@aon.at>
2025-05-15 16:46:03 +0200

    Refactor swr code

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import { useHono } from "./hooks/useHono";
 const getAllTodos = honoClient.api.todos.$get;
 
 function App() {
-  const todos = useHono({
+  const {data: todos, isLoading, error, mutate} = useHono({
     input: {},
     endpoint: getAllTodos,
   });
 
-  if (todos.isLoading) return <div>Loading...</div>;
-  if (todos.error) return <div>Error</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
 
-  const todoList = todos.data?.data.todos.map((todo) => (
+  const todoList = todos?.data.todos.map((todo) => (
     <div key={todo.id} className="todo-card">
       <h3>
         <input type="checkbox" checked={todo.done} />
