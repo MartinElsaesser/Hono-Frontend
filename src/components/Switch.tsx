3cc5ca89cace722a6df186cfe3255bf3c8193f1b
Martin Elsaesser <marels@aon.at>
2025-05-16 20:14:25 +0200

    Add sizing options to switch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -3,14 +3,16 @@ export function Switch({
 	round,
 	checked,
 	onChange,
+	size = "medium",
 }: {
 	round?: boolean;
 	checked: boolean;
 	onChange: React.ChangeEventHandler<HTMLInputElement>;
+	size: "small" | "medium";
 }) {
 	const className = round ? "slider round" : "slider";
 	return (
-		<label className="switch">
+		<label className={"switch " + size}>
 			<input type="checkbox" checked={checked} onChange={onChange} />
 			<span className={className}></span>
 		</label>
